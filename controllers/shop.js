d0655936c31f4ab70336eb0a13406c5f79f9106d
7083375809 <shaikhiliyas343>
2022-11-14 23:01:22 +0530

    Delete cart Items and Display

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,10 +53,7 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
+    .then(products => {  
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
@@ -64,8 +61,6 @@ exports.getCart = (req, res, next) => {
           });
         })
         .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
@@ -73,8 +68,10 @@ exports.postCart = (req, res, next) => {
 
   Product.findById(prodId).then(product=>{
     return req.user.addToCart(product);
+  
   }).then(result=>{
     console.log(result);
+    res.redirect('/cart');
   }).catch(err=>console.log(err));
  /* let fetchedCart;
   let newQuantity = 1;
@@ -111,14 +108,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .getCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
+    .deleteItemFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
