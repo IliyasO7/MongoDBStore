008a7f2ff8c90178e87dc0b364b67e0397370eb3
7083375809 <shaikhiliyas343>
2022-11-14 22:34:28 +0530

    1-M Cart realtion which user whcih prod which cart

--- models/user.js
+++ models/user.js
@@ -4,15 +4,40 @@ const getDb = require('../util/database').getDb
 const ObejctId = mongodb.ObjectId;
 
 class User{
-  constructor(username,email){
+  constructor(username,email, cart, id){
     this.username =username;
-    this.email = email
+    this.email = email;
+    this.cart = cart; //Object {items: []}
+    this._id = id;
   }
 
   save(){
     const db = getDb();
    return db.collection('users').insertOne(this);
 
+  }
+
+  addToCart(product){
+    const cartProductIndex = this.cart.items.findIndex(cp =>{
+      return cp.productId.toString() === product._id.toString();
+    });
+
+    let newQuantity =1;
+    const updatedCartItems = [...this.cart.items];
+
+    if(cartProductIndex>=0){
+      newQuantity = this.cart.items[cartProductIndex].quantity+1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    }else{
+      updatedCartItems.push({ productId: new ObejctId(product._id) , quantity :newQuantity})
+    }
+   
+ 
+
+    
+    const updatedCart = {items: updatedCartItems};
+    const db = getDb();
+    return db.collection('users').updateOne({_id: new ObejctId(this._id)}, {$set:{cart: updatedCart}})
   }
 
   static findById(userId){
