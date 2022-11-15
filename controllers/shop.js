ad1e3d680021924ddb721b1c48848790e6095074
7083375809 <shaikhiliyas343>
2022-11-15 16:21:39 +0530

    Complete cart and Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find() //wokrs different in mongoose
@@ -46,8 +46,11 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {  
+    .populate('cart.items.productId')
+    
+    .then(user => {
+      //console.log(user.cart.items);  
+      const products= user.cart.items;
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
@@ -102,7 +105,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
@@ -110,18 +113,36 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
+
   req.user
-   .addOrder()
+  .populate('cart.items.productId')
+  
+  .then(user => {
+    //console.log(user.cart.items);  
+    const products= user.cart.items.map(i=>{
+      return {quantity: i.quantity, product:{...i.productId._doc}};
+    });
+
+    const order = new Order({
+      user:{
+        name: req.user.name,
+        userId: req.user
+      },
+      products: products
+    })
+    return order.save();
+  })
     .then(result => {
+      return req.user.clearCart();
+  
+    }).then(result=>{
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({'user.userId': req.user._id})
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
--- routes/shop.js
+++ routes/shop.js
@@ -10,23 +10,25 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
-/*
-    
+
+router.post('/cart', shopController.postCart);
+
+router.get('/cart', shopController.getCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+
+
+
 
     
 
-    router.post('/cart', shopController.postCart);
-
-    router.get('/cart', shopController.getCart);
-
-    router.post('/cart-delete-item', shopController.postCartDeleteProduct);
-
+   
     router.post('/create-order', shopController.postOrder);
 
 
     router.get('/orders', shopController.getOrders);
 
-/*
 
 
 
@@ -35,5 +37,5 @@ router.get('/products/:productId', shopController.getProduct);
 
 
 
-*/
+
 module.exports = router;
